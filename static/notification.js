632ae6851bbd701d566e74d18266ff3338dfadbe
Oscar C <oacs1221@gmail.com>
2021-11-09 23:56:38 +0100

    patch: fix notifications styles and playback

--- static/notification.js
+++ static/notification.js
@@ -1,37 +1,54 @@
 window.onload = async () => {
   const ws = new WebSocket("ws://localhost:7001/chat");
+  // TODO preload gifs
+  const gif = new Image();
+  gif.src = "siuuu.gif";
+  const sound = new Howl({
+    src: ["siuuu.mp3"],
+  });
+  sound.play();
+
   ws.onmessage = function (event) {
     const messageEvent = JSON.parse(event.data);
     console.log({ messageEvent });
     if (messageEvent.subscription) {
-      createNotification(messageEvent);
+      if (messageEvent.subscription.type !== "channel.update") {
+      }
+      createNotification(messageEvent, sound);
     }
   };
 };
 
+const eventTextMap = {
+  "channel.update": "updated",
+  "channel.subscribe": "subscribed",
+  "channel.follow": "followed",
+};
+
 // create gif with sound and text using event data
-const createNotification = (event) => {
-  console.log(" Hola mundo");
+const createNotification = (event, sound) => {
   const notification = document.createElement("div");
+  const img = document.createElement("img");
+  img.src = "siuuu.gif";
+  img.style.width = "320px";
+  img.style.height = "240px";
+  img.classList.add("notification-gif");
   notification.classList.add("notification");
-  notification.innerHTML = `
-    <div class="notification-gif">
-        <img id="followVid" src="siuuu.gif" width="320" height="240"/>
-    </div>
+
+  notification.appendChild(img);
+  notification.innerHTML += `
     <div class="notification-text">
       <h1>${event.event["user_name"]} just ${
-    event.subscription.type.split(".")[1]
+    eventTextMap[event.subscription.type]
   } !!!</h1>
     </div>
   `;
-  const sound = new Howl({
-    src: ["siuuu.mp3"],
-  });
+
   document.body.appendChild(notification);
+
+  console.log("playin sound");
   sound.play();
-  console.log("hola");
-  setTimeout(() => {
-    console.log("adios");
+  sound.on("end", () => {
     document.body.removeChild(notification);
-  }, 5000);
+  });
 };
